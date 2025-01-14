4b9d85d556fc0c157db46da1b85145d6e665a5a6
Vimukthi Jayasanka <jayasanka1997@gmail.com>
2025-01-15 03:57:52 +0530

    Add UI componenets

---
+++ shared-app/src/main/java/lk/ijse/dep13/sharedApp/controller/MessageController.java
@@ -0,0 +1,19 @@
+package lk.ijse.dep13.sharedApp.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+
+public class MessageController {
+    public TextArea txtAreaChat;
+    public Button btnSend;
+    public TextField txtInput;
+    public Label lblH1;
+    public AnchorPane root;
+
+    public void btnSend(ActionEvent actionEvent) {
+    }
+}
