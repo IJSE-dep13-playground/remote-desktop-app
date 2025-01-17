96bb2377fa5809737b0d124348b3e840f560196e
Viyath Araliya <viyatharaliya@gmail.com>
2025-01-18 03:10:43 +0530

    handle 'btnRecieveOnAction' of controller from service layer

--- shared-app/src/main/java/lk/ijse/dep13/sharedApp/service/FileReceiverServiceImpl/FileReceiverServiceImpl01.java
+++ shared-app/src/main/java/lk/ijse/dep13/sharedApp/service/FileReceiverServiceImpl/FileReceiverServiceImpl01.java
@@ -1,11 +1,14 @@
 package lk.ijse.dep13.sharedApp.service.FileReceiverServiceImpl;
 
 import javafx.application.Platform;
+import javafx.scene.control.TextArea;
 import lk.ijse.dep13.sharedApp.service.FileReceiverService;
 
-import java.io.IOException;
+
+import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.nio.file.Paths;
 
 public class FileReceiverServiceImpl01 implements FileReceiverService {
     @Override
@@ -16,9 +19,35 @@ public class FileReceiverServiceImpl01 implements FileReceiverService {
     }
 
     @Override
-    public void recieveFileFromServer() throws IOException{
+    public void recieveFileFromServer(TextArea txtAreaRecievedFiles, Socket localSocket) throws IOException, ClassNotFoundException {
 
+            InputStream is = localSocket.getInputStream();
+            ObjectInputStream ois=new ObjectInputStream(is);
+            File file=(File) ois.readObject();
+            String ext=(String)ois.readObject();
+            String fileName=(String)ois.readObject();
+            //file.createNewFile();
+
+            String separator = Paths.get("").getFileSystem().getSeparator();
+            File fileCopy=new File(System.getProperty("user.home"),"transferredFiles"+separator+fileName+"."+ext);
+            //  String path= Files.getPA
+            //fileCopy.createNewFile();
+
+            FileInputStream fis=new FileInputStream(file);
+            FileOutputStream fos=new FileOutputStream(fileCopy);
+            while(true){
+                byte[] buffer=new byte[1024];
+                int read=fis.read(buffer);
+                if(read==-1)break;
+                fos.write(buffer,0,read);
+                // fos.flush();
+            }
+            fos.flush();
+            fos.close();
+            txtAreaRecievedFiles.setText("files copied succesfully");
+            fos.close();
+        }
     }
 
 
-}
+
